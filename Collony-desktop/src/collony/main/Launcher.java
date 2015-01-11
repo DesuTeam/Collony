d97cc7324ae057312301d35c63f114333048a920
mjdr <misterjdr@yandex.ru>
2015-01-11 21:32:59 +0200

    iouoi

--- Collony-desktop/src/collony/main/Launcher.java
+++ Collony-desktop/src/collony/main/Launcher.java
@@ -1,9 +1,20 @@
 package collony.main;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 public class Launcher 
 {
 	public static void main(String[] args) 
 	{
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		
+		cfg.width = 800;
+		cfg.height = 600;
+		cfg.useGL30 = false;
+		cfg.title = "Collony";
+		cfg.resizable = false;
+		new LwjglApplication(null,cfg);
 		
 	}
 }
