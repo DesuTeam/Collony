7ca20c57b3eaaf789d4ab395697c7d9857b61813
mjdr <misterjdr@yandex.ru>
2015-01-11 23:21:38 +0200

    create collony.util.SoundBox

---
+++ Collony-core/src/collony/util/SoundBox.java
@@ -0,0 +1,42 @@
+package collony.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.badlogic.gdx.audio.Sound;
+
+public class SoundBox 
+{
+	private static Map<String, Sound>sounds;
+	
+	static
+	{
+		sounds = new HashMap<String,Sound>();
+	}
+	
+	/** Put sound into SoundBox*/
+	public static void put(String key , Sound sound)
+	{
+		if(sound == null)
+			throw new IllegalArgumentException("Sound is null");
+		if(key == null)
+			throw new IllegalArgumentException("Key is null");
+		if(sounds.containsKey(key))
+			throw new IllegalArgumentException("Key \""+key+"\" already added!");
+		sounds.put(key, sound);
+	}
+	
+	public void play(String key)
+	{
+		if(!sounds.containsKey(key))
+			throw new IllegalArgumentException("Key \""+ key +"\" not found!");
+		sounds.get(key).play();
+	}
+	public void stop(String key)
+	{
+		if(!sounds.containsKey(key))
+			throw new IllegalArgumentException("Key \""+ key +"\" not found!");
+		sounds.get(key).stop();
+	}
+	
+}
